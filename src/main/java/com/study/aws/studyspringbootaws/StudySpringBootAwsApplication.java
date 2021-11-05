d7f6225f82b905c55e400280c1bf708fe6b1aa2b
young209 <sunsky3002@naver.com>
2021-11-05 20:52:35 +0900

    springBoot 및 gradle, git 세팅

---
+++ src/main/java/com/study/aws/studyspringbootaws/StudySpringBootAwsApplication.java
@@ -0,0 +1,13 @@
+package com.study.aws.studyspringbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBootAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/aws/studyspringbootaws/StudySpringBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.aws.studyspringbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBootAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
