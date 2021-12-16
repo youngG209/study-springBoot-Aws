4c559ae299114ee20e0e282e6aa7d831825f810f
young209 <sunsky3002@naver.com>
2021-12-16 16:32:58 +0900

    Github Action CI 설정 -> AWS 연동 nginx 무중단 배포

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,7 +13,7 @@ PROJECT_NAME=study-springBoot-Aws
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/deloy/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
